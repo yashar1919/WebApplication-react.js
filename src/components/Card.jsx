5d8a201c093357c81294b74bc68a96e11f1619c1
yashar <yashar.talebi@gmail.com>
2022-06-11 13:51:30 +0430

    add new feature

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,59 @@
 import React, { Component } from 'react';
 import styles from '../styles/Card.module.css';
+import down from "../images/down.svg";
+import up from "../images/up.svg";
+
 
 class Card extends Component {
+
+    state = {
+        counter: 0
+    }
+
     render() {
         const { image, name, price } = this.props;
+        const { counter } = this.state;
+
         return (
             <div className={styles.container}>
                 <img src={image} alt="laptop" />
                 <h3>{name}</h3>
-                <p>{price}</p>
+                {/* <p>{price} {counter > 1 && `x ${counter} = ${counter * Number(price.split(" ")[0])}$`}</p> */}
+                <p>{price} {counter > 1 ? `x ${counter} = ${counter * Number(price.split(" ")[0])}$` : ""}</p>
+
+                <div className={styles.counter}>
+                    {/* <img src={down} className={counter === 0 && styles.deactive} alt="arrowDown" onClick={this.downHandler} /> */}
+                    {<img src={down} className={counter === 0 ? styles.deactive : ""} alt="arrowDown" onClick={this.downHandler} />}
+
+                    <span>{counter}</span>
+                    <img src={up} onClick={this.upHandler} alt="arrowUp" />
+                </div>
             </div>
         );
+    }
+
+    /*  
+        ternary operator ==> {counter === 0 ? styles.deactive : ""}
+        ----------------------------------------------------------------------------------
+        and percent ==> {counter === 0 && styles.deactive}
+        shabih b ternary has amma ba && piadeSazi mishe va bara vaghtie k
+        faghat baramoon dorost boodane shart mohem bashe o ag shart false shod,
+        niaz nabashe kare khasi anjam bedim. inja ham ye shart migire o ag True bood,
+        ghesmate baad az && ro ejra mikone o ag ham False bood k hich kari nmikone.
+    */
+
+    downHandler = () => {
+        if (this.state.counter >= 1) {
+            this.setState({
+                counter: this.state.counter - 1
+            });
+        }
+    }
+
+    upHandler = () => {
+        this.setState({
+            counter: this.state.counter + 1
+        });
     }
 }
 
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,13 +8,25 @@ import zbook from '../images/zbook.jpg';
 
 
 class Cards extends Component {
+    state = {
+        laptopData: [
+            { id: 1, image: precision, name: "Dell Precision 7560", price: "4000 $" },
+            { id: 2, image: macbook, name: "MacBook Pro", price: "6100 $" },
+            { id: 3, image: zbook, name: "HP Zbook G3", price: "1800 $" },
+            { id: 4, image: alienware, name: "Dell Alienware", price: "3500 $" }
+        ]
+    }
     render() {
+        const { laptopData } = this.state;
         return (
             <div className={styles.container}>
-                <Card image={precision} name="Dell Precision 7560" price="4000 $" />
+                {/* <Card image={precision} name="Dell Precision 7560" price="4000 $" />
                 <Card image={macbook} name="MacBook Pro" price="6100 $" />
                 <Card image={zbook} name="HP Zbook G3" price="1800 $" />
-                <Card image={alienware} name="Dell Alienware" price="3500 $" />
+                <Card image={alienware} name="Dell Alienware" price="3500 $" /> */}
+
+                {/* 4khatte bala ro kholase kardim o omadim data ro rikhtim too state o hamasho yekja ba map khoondim */}
+                {laptopData.map(laptop => <Card key={laptop.id} image={laptop.image} name={laptop.name} price={laptop.price} />)}
             </div>
         );
     }
